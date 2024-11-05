5d3940e7fa07d74f163c13296a0a25943144a553
Nathan <nathaniel563@hotmail.com>
2024-11-05 13:11:48 +0400

    added front-end for home page and checkout page

---
+++ js/courses.js
@@ -0,0 +1,47 @@
+let courses = [
+    {
+        "id": 1001,
+        "subject": "Math",
+        "location": "London",
+        "price": 100.00,
+        "spaces": 5,
+        "inventory": 6,
+        "courseImage": "./images/MathsIMG.png"
+    },
+    {
+        "id": 1002,
+        "subject": "English",
+        "location": "Sharjah",
+        "price": 100.00,
+        "spaces": 8,
+        "inventory": 10,
+        "courseImage": "./images/EnglishIMG.png"
+    },
+    {
+        "id": 1003,
+        "subject": "Science",
+        "location": "Oxford",
+        "price": 100.00,
+        "spaces": 10,
+        "inventory": 3,
+        "courseImage": "./images/ScienceIMG.png"
+    },
+    {
+        "id": 1004,
+        "subject": "Music",
+        "location": "Dubai",
+        "price": 75.00,
+        "spaces": 9,
+        "inventory": 2,
+        "courseImage": "./images/MusicIMG.png"
+    },
+    {
+        "id": 1005,
+        "subject": "Sports",
+        "location": "London",
+        "price": 90.00,
+        "spaces": 5,
+        "inventory": 6,
+        "courseImage": "./images/SportsIMG.png"
+    },
+]
