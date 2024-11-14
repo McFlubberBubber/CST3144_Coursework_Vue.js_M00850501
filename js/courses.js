9309a74314833959d1631c11d78b804e13eb149a
Nathan <nathaniel563@hotmail.com>
2024-11-14 13:10:15 +0400

    added extra courses to main page, updated checkout form

--- js/courses.js
+++ js/courses.js
@@ -44,4 +44,49 @@ let courses = [
         "inventory": 6,
         "courseImage": "./images/SportsIMG.png"
     },
+    {
+        "id": 1006,
+        "subject": "Sports 2",
+        "location": "London",
+        "price": 90.00,
+        "spaces": 5,
+        "inventory": 8,
+        "courseImage": "./images/SportsIMG.png"
+    },
+    {
+        "id": 1007,
+        "subject": "English 2",
+        "location": "London",
+        "price": 100.00,
+        "spaces": 5,
+        "inventory": 9,
+        "courseImage": "./images/EnglishIMG.png"
+    },
+    {
+        "id": 1008,
+        "subject": "Maths 2",
+        "location": "Dubai",
+        "price": 50.00,
+        "spaces": 8,
+        "inventory": 10,
+        "courseImage": "./images/MathsIMG.png"
+    },
+    {
+        "id": 1009,
+        "subject": "Science 2",
+        "location": "Amsterdam",
+        "price": 75.00,
+        "spaces": 7,
+        "inventory": 5,
+        "courseImage": "./images/ScienceIMG.png"
+    },
+    {
+        "id": 1010,
+        "subject": "English 3",
+        "location": "London",
+        "price": 40.00,
+        "spaces": 5,
+        "inventory": 9,
+        "courseImage": "./images/EnglishIMG.png"
+    },
 ]
